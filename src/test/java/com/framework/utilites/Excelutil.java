a58cfce6133a9da736b617991f589a0c3fba16bd
SumitSagar1708 <sumit.sagar.108@gmail.com>
2025-01-30 22:59:21 +0530

    DB

--- src/test/java/com/framework/utilites/Excelutil.java
+++ src/test/java/com/framework/utilites/Excelutil.java
@@ -12,33 +12,31 @@ import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
 public class Excelutil {
-	public static List<Map<String,String>> readExcelData (String fileName, String sheetName) {
-		List<Map<String,String>> data = new ArrayList<Map<String,String>>();
-		
-		try {
-			FileInputStream file = new FileInputStream(System.getProperty("user.dir") + "\\TestData\\" + fileName); 
-			XSSFWorkbook workbook = new XSSFWorkbook(file);
-			XSSFSheet sheet = workbook.getSheet(sheetName); 
-			int totalRows = sheet.getPhysicalNumberOfRows(); 
-			int totalColumns = sheet.getRow(0).getPhysicalNumberOfCells();
-			
-			for(int r=1;r<totalRows ; r++) {				
-				HashMap<String,String> rowData = new HashMap<String,String>(); 				
-				for(int c=0; c<totalColumns;c++) {
-					String columnName = sheet.getRow(0).getCell(c).getStringCellValue();
-					String columnValue = sheet.getRow(r).getCell(c).getStringCellValue();
-					rowData.put(columnName, columnValue);
-				}
-				data.add(rowData);
-			}			
-		} catch (FileNotFoundException e) {
-			e.printStackTrace();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-		
-		
-		return data;		
-	}
+	    // Method to read Excel data into a List of Maps
+	    public static List<Map<String, String>> readExcelData(String fileName, String sheetName) {
+	        List<Map<String, String>> data = new ArrayList<>();
+	        
+	        try (FileInputStream file = new FileInputStream(System.getProperty("user.dir") + "\\TestData\\" + fileName);
+	             XSSFWorkbook workbook = new XSSFWorkbook(file)) {
 
-}
+	            XSSFSheet sheet = workbook.getSheet(sheetName);
+	            int totalRows = sheet.getPhysicalNumberOfRows();
+	            int totalColumns = sheet.getRow(0).getPhysicalNumberOfCells();
+
+	            // Iterate through rows (starting from 1 to skip the header)
+	            for (int r = 1; r < totalRows; r++) {
+	                Map<String, String> rowData = new HashMap<>();
+	                for (int c = 0; c < totalColumns; c++) {
+	                    String columnName = sheet.getRow(0).getCell(c).getStringCellValue();
+	                    String columnValue = sheet.getRow(r).getCell(c) != null ? sheet.getRow(r).getCell(c).getStringCellValue() : "";
+	                    rowData.put(columnName, columnValue);
+	                }
+	                data.add(rowData);
+	            }
+	        } catch (IOException e) {
+	            e.printStackTrace();
+	        }
+
+	        return data;
+	    }
+	}
--- src/test/java/com/framework/utilites/ReadExcel.java
+++ src/test/java/com/framework/utilites/ReadExcel.java
@@ -3,21 +3,19 @@ package com.framework.utilites;
 import java.util.List;
 import java.util.Map;
 
+
 public class ReadExcel {
 
 public static void main(String[] args) {
-		
-		//Read Excel into 2d array
-		String [][] data = Excelutil.readData("ProductDetails.xlsx", "Sheet1");
-		
-		//Read Excel into List
-				List<Map<String,String>> excelData = Excelutil.readData("CollectionsAssignment.xlsx", "ProductDetails");
-				
-				System.out.println(excelData.get(2));
-		
-		System.out.println(excelData.get(2));
-		
+	
+        // Read Excel into List
+	List<Map<String, String>> Data = Excelutil.readExcelData("ProductDetails.xlsx", "Sheet1");
+	
+	for (String cell : Data.get(2)) {
+        System.out.print(cell + " ");
 
+    
+        
 	}
-
+    }
 }
